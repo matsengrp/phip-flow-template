a74ad27f9b7961a0ffc4c82cacd7c539434a0b67
jgallowa07 <jaredgalloway07@gmail.com>
2021-03-28 13:31:05 -0700

    removed submidules

--- run_phip_flow.sh
+++ run_phip_flow.sh
@@ -8,7 +8,7 @@ module load Singularity
 export PATH=$SINGULARITYROOT/bin/:$PATH
 
 /usr/bin/time nextflow  \
-    -C phipflow.config.best \
-    run ../../../../phip-flow/PhIP-Flow.nf \
+    -C phipflow.config \
+    run phip-flow/PhIP-Flow.nf \
     -with-report output_local/nextflow_report.html \
     -work-dir /fh/scratch/delete30/matsen_e/jgallowa/temp/work/ \
