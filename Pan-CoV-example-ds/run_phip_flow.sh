e0382839432bad322b921079c6b8a8eb0de638e5
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-19 10:28:08 -0700

    updated template, slim down annotations

--- Pan-CoV-example-ds/run_phip_flow.sh
+++ Pan-CoV-example-ds/run_phip_flow.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
 
-set -e
-#source /app/lmod/lmod/init/profile
-#
-#module load nextflow
-#module load Singularity
-#export PATH=$SINGULARITYROOT/bin/:$PATH
+set -eu
 
 /usr/bin/time nextflow  \
-    -C phipflow_docker.config \
-    run ../phip-flow/PhIP-Flow.nf \
-    -with-report ./output/nextflow_report.html \
-    -work-dir ./output/work/ \
-    -resume
+  -C phipflow_docker.config \
+  run matsengrp/phip-flow/PhIP-Flow.nf \
+  -with-report ./output/nextflow_report.html \
+  -work-dir ./output/work/ \
+  -resume
