47fb2ac54c1a85dc4974c0e4c5816bcf437c3c8b
Jared Galloway <jaredgalloway07@gmail.com>
2021-10-20 05:28:13 -0700

    updated sample table

--- Pan-CoV-example-ds/run_phip_flow.sh
+++ Pan-CoV-example-ds/run_phip_flow.sh
@@ -4,7 +4,8 @@ set -eu
 
 /usr/bin/time nextflow  \
   -C phipflow_docker.config \
-  run matsengrp/phip-flow/PhIP-Flow.nf \
+  #run ../phip-flow/PhIP-Flow.nf
+  run matsengrp/phip-flow/PhIP-Flow.nf -r main\
   -with-report ./output/nextflow_report.html \
   -work-dir ./output/work/ \
   -resume
